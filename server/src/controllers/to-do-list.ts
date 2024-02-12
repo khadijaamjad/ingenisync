0b13371ee83199cb3de9df3f84a3942144db307f
khadijaamjad <khadijah.amjad25@gmail.com>
2024-02-12 11:34:37 +0500

    Created API endpoints for upcoming tasks page.

--- server/src/controllers/to-do-list.ts
+++ server/src/controllers/to-do-list.ts
@@ -1,4 +1,7 @@
+import { TaskTimelineEnum } from '../models/internal';
 import { ToDoList } from '../models/to-do-list';
+
+const HOURS_TO_MS = 24 * 60 * 60 * 1000;
 
 export const add = async (req, res) => {
   const body = req.body;
@@ -54,4 +57,86 @@ export const getAll = async (req, res) => {
   } catch (error) {
     res.status(500).send(error);
   }
+};
+
+export const getItemsDueThisWeek = async (req, res) => {
+  try {
+    const result = await getAggregatedResult(TaskTimelineEnum.ThisWeek);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const getItemsDueToday = async (req, res) => {
+  try {
+    const result = await getAggregatedResult(TaskTimelineEnum.Today);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const getItemsDueTomorrow = async (req, res) => {
+  try {
+    const result = await getAggregatedResult(TaskTimelineEnum.Tomorrow);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const getAggregatedResult = async (taskTimeline: TaskTimelineEnum) => {
+  let aggregateCondition = {};
+  const today = new Date(new Date().setHours(0, 0, 0, 0));
+  const tomorrowStart = new Date(today.getTime() + HOURS_TO_MS);
+  const nextDayStart = new Date(tomorrowStart.getTime() + HOURS_TO_MS);
+  const weekEnd = new Date(new Date().getTime() + 7 * HOURS_TO_MS);
+
+  switch (taskTimeline) {
+    case TaskTimelineEnum.Today:
+      aggregateCondition = {
+        'toDoItems.dueDate': { $gte: today, $lt: tomorrowStart }
+      };
+      break;
+    case TaskTimelineEnum.Tomorrow:
+      aggregateCondition = {
+        'toDoItems.dueDate': { $gte: tomorrowStart, $lt: nextDayStart }
+      };
+      break;
+    case TaskTimelineEnum.ThisWeek:
+      aggregateCondition = {
+        $and: [
+          { 'toDoItems.dueDate': { $gte: today } },
+          { 'toDoItems.dueDate': { $lt: weekEnd } }
+        ]
+      };
+      break;
+  }
+
+  const tasksDue = await ToDoList.aggregate([
+    {
+      // Match lists and items based on need
+      $match: aggregateCondition
+    },
+    {
+      // Unwind the "toDoItems" array to process each item independently
+      $unwind: '$toDoItems'
+    },
+    {
+      // Project the required fields
+      $project: {
+        title: '$title',
+        description: '$toDoItems.description',
+        dueDate: '$toDoItems.dueDate'
+      }
+    },
+    {
+      // Sort the results by due date for convenience
+      $sort: { dueDate: 1 } // Ascending order
+    }
+  ]);
+
+  console.log(JSON.stringify(aggregateCondition));
+  return tasksDue;
 };
--- server/src/routers/to-do-list.ts
+++ server/src/routers/to-do-list.ts
@@ -5,11 +5,20 @@ import {
   deleteSingle,
   getSingle,
   getAll,
+  getItemsDueThisWeek,
+  getItemsDueToday,
+  getItemsDueTomorrow
 } from '../controllers/to-do-list';
 
 const router: Router = express.Router();
 
 router.post('/', add);
+
+router.get('/week', getItemsDueThisWeek);
+
+router.get('/today', getItemsDueToday);
+
+router.get('/tomorrow', getItemsDueTomorrow);
 
 router.get('/:id', getSingle);
 
